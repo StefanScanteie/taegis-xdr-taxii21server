8b572d631979c21ca04d6258e41292e2c217f1f8
StefanScanteie <stefan.angel5@gmail.com>
2025-05-26 17:58:37 +0300

    Add files via upload

---
+++ test_taxii.py
@@ -0,0 +1,21 @@
+from taxii2client.v21 import Server
+from requests.auth import HTTPBasicAuth
+
+server = Server(
+    'https://carried-lonely-design-bent.trycloudflare.com/taxii2/',
+    auth=HTTPBasicAuth('admin', 'adminpass'),
+    verify=False  # Set to True if using HTTPS with valid cert
+)
+
+# Print API roots
+print("API Roots:")
+for api_root in server.api_roots:
+    print("-", api_root.url)
+
+# Get the default root
+api_root = server.api_roots[0]
+
+# Get available collections
+print("\nCollections:")
+for collection in api_root.collections:
+    print("-", collection.title, f"(ID: {collection.id})")
